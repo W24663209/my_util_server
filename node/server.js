75be0801e6d7942add9a77b6e23e4755e0b8f776
weizongtang <24663209@qq.com>
2022-09-07 14:29:30 +0800

    move

---
+++ node/server.js
@@ -0,0 +1,48 @@
+const curlconverter = require('curlconverter');
+const url = require("url");
+const util = require('util');
+const querystring = require('querystring');
+var http = require('http');
+
+// 创建服务器，使用createServer方法
+var server = http.createServer(function (req, res) {
+    res.statusCode = 200
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    //允许的header类型
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    //跨域允许的请求方式
+    res.setHeader("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method === 'GET') {
+        toGet(req, res);
+    } else if (req.method === 'POST') {
+        toPost(req, res);
+    } else if (req.method === 'OPTIONS') {
+        res.end('');
+    }
+}).listen(3000);
+
+function toGet(req, res) {
+    let data = 'GET请求内容：\n' + util.inspect(url.parse(req.url));
+    res.end('成功');
+    console.log(data);
+}
+
+function toPost(req, res) {
+    // 定义了一个data变量，用于暂存请求体的信息
+    let data = '';
+    // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+    req.on('data', function (chunk) {
+        data += chunk;
+    });
+    let params = querystring.parse(url.parse(req.url).query)
+    // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+    req.on('end', function () {
+        res.end(eval(params['method'])(JSON.parse(data)));
+    });
+}
+
+function toCurl(data) {
+    let language = data['language'];
+    let value = data['value'];
+    return curlconverter[language](value)
+}
--- py/test.py
+++ py/test.py
@@ -1,5 +1,24 @@
-import time
+# const val RSSI_1M: Double = 50.0//发射端和接收端相隔1米时的信号强度
+# const val N_VALUE: Double = 2.5//环境衰减因子
+#
+# /**
+#  * 根据rssi值估算出距离
+#  * @param rssi信号强度
+#  * @return 距离(单位：米)
+#  */
+# fun getDistance(rssi: Int): Double {
+#     val rssiAbs = Math.abs(rssi)
+#     val power = (rssiAbs - RSSI_1M) / (10 * N_VALUE)
+#     return Math.pow(10.0, power)
+# }
+import math
 
-start_time = time.time()
-time.sleep(1)
-print(time.time()-start_time)
+RSSI_1M = 50.0
+N_VALUE = 2.5
+
+def getDistance(rssi):
+    rssiAbs = math.fabs(rssi)
+    power = (rssiAbs - RSSI_1M) / (10 * N_VALUE)
+    return math.pow(10.0,power)
+
+print(getDistance(-68.006000))
